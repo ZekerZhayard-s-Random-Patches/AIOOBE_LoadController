ca42e1da3ba783309c0a328bb18a5ed8dfa82928
ZekerZhayard <trees1000@qq.com>
2023-02-15 17:13:33 +0800

    1.1

--- src/main/java/io/github/zekerzhayard/aioobe_loadcontroller/core/FMLLoadingPlugin.java
+++ src/main/java/io/github/zekerzhayard/aioobe_loadcontroller/core/FMLLoadingPlugin.java
@@ -1,10 +1,24 @@
 package io.github.zekerzhayard.aioobe_loadcontroller.core;
 
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Map;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
+@IFMLLoadingPlugin.TransformerExclusions("io.github.zekerzhayard.aioobe_loadcontroller.core.")
 public class FMLLoadingPlugin implements IFMLLoadingPlugin {
+    static {
+        try {
+            Method addURLMethod = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            addURLMethod.setAccessible(true);
+            addURLMethod.invoke(ClassLoader.getSystemClassLoader(), FMLLoadingPlugin.class.getProtectionDomain().getCodeSource().getLocation());
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
+
     @Override
     public String[] getASMTransformerClass() {
         return new String[] { "io.github.zekerzhayard.aioobe_loadcontroller.core.ClassTransformer" };
